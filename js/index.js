39c0143f592955721d658da73f9862deff4f7b20
atul aditya <atul100aditya@gmail.com>
2021-01-14 15:20:42 +0530

    Basics of JS

---
+++ js/index.js
@@ -0,0 +1,19 @@
+console.log(x);
+console.log(getName);
+console.log(getName2);
+console.log(getName3);
+getName;
+
+
+var x = 7;
+function getName() {
+  console.log("hello js");
+}
+
+var getName2 = function (){
+  console.log("hello js 2");
+}
+
+var getName3 = () => {
+  console.log("hello js 2");
+}
